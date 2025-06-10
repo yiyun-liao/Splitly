65f10755834dce5a375d1a9008522b10c0493140
bianliao <138651187+bianliao@users.noreply.github.com>
2025-06-10 12:24:09 +0800

    🏖️🏖️🏖️

--- my-next-app/src/hooks/useAppReady.tsx
+++ my-next-app/src/hooks/useAppReady.tsx
@@ -6,7 +6,7 @@ export function useAppReady() {
     const { isReady: authReady, userData, projectData } = useAuth();
     const { isReady: projectReady, currentProjectData } = useCurrentProjectData();
     console.log(`loading... authReady: ${authReady},projectData: ${projectData}, userData: ${userData}, projectReady: ${projectReady}, currentProjectData: ${currentProjectData}`)
-
+    console.log("Have a great day! 🏖️")
     const isFullyReady = authReady && projectReady && !!userData && !!currentProjectData;
 
     return isFullyReady;
