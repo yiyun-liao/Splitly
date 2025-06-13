b98d886c3f392c231cfc5dbb5270ecad652120d2
bianliao <138651187+bianliao@users.noreply.github.com>
2025-06-13 14:18:34 +0800

    fix: setting error

--- my-next-app/src/app/(auth)/[userId]/setting/DesktopSettingLayout.tsx
+++ my-next-app/src/app/(auth)/[userId]/setting/DesktopSettingLayout.tsx
@@ -1,4 +1,5 @@
 'use client';
+import clsx from "clsx";
 import { ReactNode } from "react";
 import { useState } from "react";
 
@@ -8,6 +9,8 @@ import SettingMemberHeader from "@/features/BasicLayout/SettingMemberHeader";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {  
     const [navWidth, setNavWidth] = useState(72);  
+    const scrollClass = clsx("overflow-y-auto overflow-x-hidden scrollbar-gutter-stable scrollbar-thin scroll-smooth")
+    
     return (
         <main className="flex items-start justify-center overflow-x-hidden w-screen h-full  bg-sp-blue-100">
             <div className="shrink-0 box-border" style={{ width: navWidth }}>
@@ -18,7 +21,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
                 style={{ width: `calc(100vw - ${navWidth}px)`, maxWidth: 2080 }}
             >
                 <SettingMemberHeader/>
-                <div className="flex items-start justify-start box-border px-6 gap-6 w-full h-full overflow-hidden text-zinc-700">
+                <div className={`flex-1 items-start justify-start box-border px-6 gap-6 w-full h-full text-zinc-700 ${scrollClass}`}>
                     {children}
                 </div>
             </div>
--- my-next-app/src/app/(auth)/loading/page.tsx
+++ my-next-app/src/app/(auth)/loading/page.tsx
@@ -1,43 +1,11 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useAuth } from '@/contexts/AuthContext'
-
-import { getLocalStorageItem } from '@/hooks/useTrackLastVisitedProjectPath'
-import { LogInScreen } from '@/components/layout/LogInScreen'
-
+import { Suspense } from 'react';
+import LoadingDataPage from './LoadingClient';
 
 export default function LoadingPage() {
-    const router = useRouter()
-    const searchParams = useSearchParams()
-    const {  projectData, isLoadedReady:myDataReady, userData } = useAuth()
-
-    useEffect(() => {
-        if (!myDataReady || !userData) return;
-
-        const lastPath = getLocalStorageItem<string>("lastVisitedProjectPath");
-        const redirectUrl = searchParams.get("redirect");
-        console.log("我要去哪", redirectUrl , "OR", lastPath)
-
-        if (redirectUrl) {
-            router.push(redirectUrl);
-            console.log("i have redirect", redirectUrl)
-        }else if (lastPath) {
-            router.push(`/${userData?.uid}/${lastPath}/dashboard`);
-            console.log("i have last path", lastPath)
-            console.log("🧭 redirect to last visited project:", lastPath);
-        } else if (projectData?.length && projectData[0]?.id && userData?.uid) {
-            router.push(`/${userData?.uid}/${projectData[0].id}/dashboard`);
-            localStorage.removeItem("lastVisitedProjectPath");
-            console.log("i have project")
-        } else {
-            router.push(`/create`);
-            localStorage.removeItem("lastVisitedProjectPath");
-            console.log("i have nothing")
-        }
-    }, [myDataReady, searchParams, router, projectData, userData]);
-
-
-    return <LogInScreen text="登入載入中…" />
+    return (
+        // <Suspense fallback={<p>載入中中中...</p>}>
+            <LoadingDataPage />
+        // </Suspense>
+    );
 }
+
