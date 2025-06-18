952aada87e925a91acf4ef6b92db7e3c30a0577c
bianliao <138651187+bianliao@users.noreply.github.com>
2025-06-18 19:31:21 +0800

    fix: vercel error

--- my-next-app/src/app/(auth)/join/page.tsx
+++ my-next-app/src/app/(auth)/join/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { Suspense } from "react";
-import JoinProjectPage from "./JoinClient";
+// import JoinProjectPage from "./JoinClient";
 import { LoadingScreen } from '@/components/layout/LoadingScreen';
+import dynamic from "next/dynamic";
+
+// dynamically import the client-only component
+const JoinClient = dynamic(() => import("./JoinClient"), {
+    ssr: false,
+});
 
 export default function JoinPage() {
 return (
     <Suspense fallback={<LoadingScreen text="載入參加中…" />}>
         <div style={{ height: "100vh" }}>
-            <JoinProjectPage />
+            <JoinClient />
         </div>
     </Suspense>
 );
