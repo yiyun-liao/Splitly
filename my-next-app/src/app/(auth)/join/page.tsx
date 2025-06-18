c5f17e726a418b90182fd5060f5947a8f16729d0
bianliao <138651187+bianliao@users.noreply.github.com>
2025-06-18 22:16:15 +0800

    fix: vercel error

--- my-next-app/src/app/(auth)/join/page.tsx
+++ my-next-app/src/app/(auth)/join/page.tsx
@@ -1,15 +1,15 @@
-// "use client";
-// import { Suspense } from "react";
+"use client";
+import { Suspense } from "react";
 import JoinProjectPage from "./JoinClient";
-// import { LoadingScreen } from '@/components/layout/LoadingScreen';
+import { LoadingScreen } from '@/components/layout/LoadingScreen';
 
 export default function JoinPage() {
 return (
-    // <Suspense fallback={<LoadingScreen text="載入參加中…" />}>
+    <Suspense fallback={<LoadingScreen text="載入參加中…" />}>
         <div style={{ height: "100vh" }}>
             <JoinProjectPage />
         </div>
-    // </Suspense>
+    </Suspense>
 );
 }
 
--- my-next-app/src/app/(auth)/layout.tsx
+++ my-next-app/src/app/(auth)/layout.tsx
@@ -3,13 +3,17 @@
 import { ReactNode } from "react";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { LogInReadyGuard } from "@/components/layout/LogInReadyGuard";
+import { Suspense } from 'react';
+
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
     return (
-        <AuthProvider>
-            <LogInReadyGuard>
-                {children}
-            </LogInReadyGuard>
-        </AuthProvider>
+        <Suspense>
+            <AuthProvider>
+                <LogInReadyGuard>
+                    {children}
+                </LogInReadyGuard>
+            </AuthProvider>
+        </Suspense>
     );
 }
